e2a83363f45b91eb0e63b19c115f2bd2620c2e80
Vishnu Singh <vs414447@gmail.com>
2026-04-25 17:41:05 +0530

    Adding Github Component that displays Projects in cards layout within a window

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./app.scss";
 import Dock from "./components/Dock";
 import Navbar from "./components/Navbar";
-import MacWindow from "./components/windows/MacWindow";
+import Github from "./components/windows/Github";
 
 function App() {
 
@@ -9,12 +9,9 @@ function App() {
     <>
       <main>
         <Navbar />
-        <MacWindow>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus soluta iusto officiis totam tenetur voluptates dolores assumenda odit, possimus, a esse? Labore consectetur pariatur provident et numquam quaerat dolorum unde.
-          </p>
-        </MacWindow>
         <Dock />
+        
+        <Github />
       </main>
     </>
   )
